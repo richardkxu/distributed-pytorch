7b927389311091eaea98764a8b18c84e1a3f14f0
richardkxu <richardkxu@gmail.com>
2020-01-28 16:57:22 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# Distributed training with PyTorch
+
+## Overview
+
+We will cover the following training methods for PyTorch:
+- regular, single node, single GPU training
+- `torch.nn.DataParallel`
+- `torch.nn.DistributedDataParallel`
+- mixed precision training
+
+We will cover the following use cases:
+- Single node single GPU training
+- Single node multi-GPU training
+- Multi-node multi-GPU training
+
+## Requirements
+
+- Install PyTorch ([pytorch.org](http://pytorch.org)) with GPU
+- Download the ImageNet dataset and move validation images to labeled subfolders
+    - To do this, you can use the following script: https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh
+    - on HAL cluster, use `/home/shared/imagenet/raw/`
+
+## Non-distributed (ND) training
+
+Use cases:
+- Single node single GPU training
+
+This is the most basic training method, no data parallel at all
+
+```bash
+python imagnet_nd.py -a resnet50 /home/shared/imagenet/raw/
+```
+
+The default learning rate schedule starts at 0.1 and decays by a factor of 10 every 30 epochs. This is appropriate for ResNet and models with batch normalization, but too high for AlexNet and VGG. Use 0.01 as the initial learning rate for AlexNet or VGG:
+
+```bash
+python imagnet_nd.py -a alexnet --lr 0.01 /home/shared/imagenet/raw/
+```
+
+## Single-processing Data Parallel (DP)
+
+Use cases:
+- Single node multi-GPU training
+
+References:
+
+- https://pytorch.org/tutorials/beginner/blitz/data_parallel_tutorial.html
+
+`torch.nn.DataParallel` is easier to use (just wrap the model and run your training script). However, because it uses one process to compute the model weights and then distribute them to each GPU on the current node during each batch, networking quickly becomes a bottle-neck and GPU utilization is often very low. Furthermore, it requires that all the GPUs be on the same node and doesn’t work with `Apex` for mixed-precision training.
+
+## Multi-processing Distributed Data Parallel (DDP)
+
+Use cases:
+- Single node multi-GPU training
+- Multi-node multi-GPU training
+
+References: 
+- https://yangkky.github.io/2019/07/08/distributed-pytorch-tutorial.html
+- https://github.com/yangkky/distributed_tutorial/blob/master/src/mnist-distributed.py
+- https://github.com/pytorch/examples/blob/master/imagenet/main.py
+- https://medium.com/intel-student-ambassadors/distributed-training-of-deep-learning-models-with-pytorch-1123fa538848
+- http://www.telesens.co/2019/04/04/distributed-data-parallel-training-using-pytorch-on-aws/
+- http://seba1511.net/dist_blog/
+
+`torch.nn.DistributedDataParallel` is the recommeded way of doing distributed training in PyTorch. It is proven to be significantly faster than `torch.nn.DataParallel` for single-node multi-GPU data parallel training. `nccl` backend is currently the fastest and highly recommended backend to be used with distributed training and this applies to both single-node and multi-node distributed training.
+
+Multiprocessing with DistributedDataParallel duplicates the model on each GPU on each compute node. The GPUs can all be on the same node or spread across multiple nodes. If you have 2 computer nodes with 4 GPUs each, you have a total of 8 model replicas. Each replica is controlled by one process and handles a portion of the input data.  Every process does identical tasks, and each process communicates with all the others. During the backwards pass, gradients from each node are averaged. Only gradients are passed between the processes/GPUs so that network communication is less of a bottleneck.
+
+During training, each process loads its own minibatches from disk and passes them to its GPU. Each GPU does its own forward pass, and then the gradients are all-reduced across the GPUs. Gradients for each layer do not depend on previous layers, so the gradient all-reduce is calculated concurrently with the backwards pass to futher alleviate the networking bottleneck. At the end of the backwards pass, every node has the averaged gradients, ensuring that the model weights stay synchronized.
+
+All this requires that the multiple processes, possibly on multiple nodes, are synchronized and communicate. Pytorch does this through its `distributed.init_process_group` function. This function needs to know where to find process 0 so that all the processes can sync up and the total number of processes to expect. Each individual process also needs to know the total number of processes as well as its rank within the processes and which GPU to use. It’s common to call the total number of processes the `world size`. Finally, each process needs to know which slice of the data to work on so that the batches are non-overlapping. Pytorch provides `nn.utils.data.DistributedSampler` to accomplish this.
+
+
+### Single node, multiple GPUs:
+
+```bash
+python  imagenet_ddp.py -a resnet50 --dist-url 'tcp://MASTER_IP:MASTER_PORT' --dist-backend 'nccl' --world-size 1 --rank 0 --desired-acc 0.75 /home/shared/imagenet/raw/
+```
+
+### Multiple nodes, multiple GPUs:
+
+To run your programe on 4 nodes with 4 GPU each, you will need to open 4 terminals and run slightly different command on each node.
+
+Node 0:
+```bash
+python  imagenet_ddp.py -a resnet50 --dist-url 'tcp://MASTER_IP:MASTER_PORT' --dist-backend 'nccl' --world-size 4 --rank 0 --desired-acc 0.75 /home/shared/imagenet/raw/
+```
+
+- `MASTER_IP`: IP address for the master node of your choice
+- `MASTER_PORT`: open port number on the master node. if you don't know, use `8888`
+- `--world-size`: equals the # of compute nodes you are using
+- `--rank`: rank of the current node, should be an int between `0` and `--world-size - 1`
+- `--desired-acc`: desired accuracy to stop training
+
+Node 1:
+```bash
+python  imagenet_ddp.py -a resnet50 --dist-url 'tcp://MASTER_IP:MASTER_PORT' --dist-backend 'nccl' --world-size 4 --rank 1 --desired-acc 0.75 /home/shared/imagenet/raw/
+```
+
+Node 2:
+```bash
+python  imagenet_ddp.py -a resnet50 --dist-url 'tcp://MASTER_IP:MASTER_PORT' --dist-backend 'nccl' --world-size 4 --rank 2 --desired-acc 0.75 /home/shared/imagenet/raw/
+```
+
+Node 3:
+```bash
+python  imagenet_ddp.py -a resnet50 --dist-url 'tcp://MASTER_IP:MASTER_PORT' --dist-backend 'nccl' --world-size 4 --rank 3 --desired-acc 0.75 /home/shared/imagenet/raw/
+```
+
+## FP16 and FP32 mixed precision training with NVIDIA `Apex`
+
+References:
+
+- mnist apex: https://github.com/yangkky/distributed_tutorial/blob/master/src/mnist-mixed.py
+- apex examples: https://github.com/nvidia/apex/tree/master/examples
+- apex tutorial: https://devblogs.nvidia.com/apex-pytorch-easy-mixed-precision-training/
+- apex tutorial: https://developer.nvidia.com/automatic-mixed-precision
+- apex doc: https://docs.nvidia.com/deeplearning/sdk/mixed-precision-training/index.html
+
+Mixed precision training: majority of the network uses FP16 arithmetic, while automatically casting potentially unstable operations to FP32.
+
+Advantages:
+- reducing memory storage/bandwidth demands
+- use larger batch sizes
+- take advantage of NVIDIA Tensor Cores for gemms and convolutions
+
+Use `imagenet_ddp_mixprec.py` for training. It is run the same way as the DDP training script.
+
+
+## Distributed training with `Horovod`
+
+References:
+
+- https://docs.databricks.com/applications/deep-learning/distributed-training/mnist-pytorch.html
+- https://horovod.readthedocs.io/en/latest/pytorch.html
+- https://github.com/horovod/horovod/blob/master/docs/pytorch.rst
+
+## Acknowledgment
+
+Shout-out to all the references, blogs, code samples, figures... used in this tutorial!
---
+++ imagenet_ddp.py
@@ -0,0 +1,399 @@
+import argparse
+import os
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.multiprocessing as mp
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet50)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=1024, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 1024), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--world-size', default=-1, type=int,
+                    help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int,
+                    help='node rank for distributed training')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training. This should be'
+                         'the IP address and open port number of the master node')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
+parser.add_argument('--desired-acc', default=None, type=float,
+                    help='Training will stop after desired-acc is reached.')
+
+best_acc1 = 0
+
+
+def main():
+    args = parser.parse_args()
+
+    ############################################################
+    ngpus_per_node = torch.cuda.device_count()
+
+    # on each node we have: ngpus_per_node processes and ngpus_per_node gpus
+    # that is, 1 process for each gpu on each node.
+    # world_size is the total number of processes to run
+    args.world_size = ngpus_per_node * args.world_size
+
+    # Use torch.multiprocessing.spawn to launch distributed processes: the
+    # main_worker process function
+    mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    ############################################################
+
+
+def main_worker(gpu, ngpus_per_node, args):
+    """
+    :param gpu: this is the process index, mp.spawn will assign this for you, goes from 0 to ngpus - 1 for the curr node
+    :param ngpus_per_node:
+    :param args:
+    :return:
+    """
+    global best_acc1
+    print("Use GPU: {} for training".format(gpu))
+
+    ############################################################
+    # For multiprocessing distributed training, rank needs to be the
+    # global rank among all the processes across all nodes
+    # This is “blocking,” meaning that no process will continue until all processes have joined.
+    args.rank = args.rank * ngpus_per_node + gpu
+    dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                            world_size=args.world_size, rank=args.rank)
+    ############################################################
+
+    # create model
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        model = models.__dict__[args.arch](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        model = models.__dict__[args.arch]()
+
+    ############################################################
+    # For multiprocessing distributed, DistributedDataParallel constructor
+    # should always set the single device scope, otherwise,
+    # DistributedDataParallel will use all available devices.
+    torch.cuda.set_device(gpu)
+    model.cuda(gpu)
+    # When using a single GPU per process and per
+    # DistributedDataParallel, we need to divide the batch size
+    # ourselves based on the total number of GPUs we have
+    args.batch_size = int(args.batch_size / ngpus_per_node)  # calculate local batch size for each GPU
+    args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+    ############################################################
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(gpu)
+
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            # Map model to be loaded to specified single gpu.
+            loc = 'cuda:{}'.format(gpu)
+            checkpoint = torch.load(args.resume, map_location=loc)
+            args.start_epoch = checkpoint['epoch']
+            best_acc1 = checkpoint['best_acc1']
+            # best_acc1 may be from a checkpoint from a different GPU
+            best_acc1 = best_acc1.to(gpu)
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    # cudnn will look for the optimal set of algorithms for that
+    # particular configuration. this will have faster runtime if
+    # your input sizes does not change at each iteration
+    cudnn.benchmark = True
+
+    # Data loading code
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    ############################################################
+    # makes sure that each process gets a different slice of the training data
+    # during distributed training
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+
+    # notice we turn off shuffling and use distributed data sampler
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+    ############################################################
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
+
+    end = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        train_sampler.set_epoch(epoch)
+        adjust_learning_rate(optimizer, epoch, args)
+
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch, gpu, args)
+
+        # evaluate on validation set
+        acc1 = validate(val_loader, model, criterion, gpu, args)
+
+        # remember best acc@1 and save checkpoint
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        if args.rank % ngpus_per_node == 0:
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'state_dict': model.state_dict(),
+                'best_acc1': best_acc1,
+                'optimizer': optimizer.state_dict(),
+            }, is_best)
+
+        # stop training once reach desired accuracy
+        if args.desired_acc and best_acc1 >= args.desired_acc:
+            time_elapsed = time.time() - end
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'state_dict': model.state_dict(),
+                'best_acc1': best_acc1,
+                'optimizer': optimizer.state_dict(),
+                'training_time': time_elapsed,
+            }, is_best)
+            print("Reached acc of: {:6.2f}; Time elapsed: {:6.3f}".format(args.desired_acc, time_elapsed))
+            break
+
+
+def train(train_loader, model, criterion, optimizer, epoch, gpu, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (images, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        images = images.cuda(gpu, non_blocking=True)
+        target = target.cuda(gpu, non_blocking=True)
+
+        # compute output
+        output = model(images)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), images.size(0))
+        top1.update(acc1[0], images.size(0))
+        top5.update(acc5[0], images.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.display(i)
+
+
+def validate(val_loader, model, criterion, gpu, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(val_loader),
+        [batch_time, losses, top1, top5],
+        prefix='Test: ')
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            images = images.cuda(gpu, non_blocking=True)
+            target = target.cuda(gpu, non_blocking=True)
+
+            # compute output
+            output = model(images)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+            top5.update(acc5[0], images.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+if __name__ == '__main__':
+    main()
